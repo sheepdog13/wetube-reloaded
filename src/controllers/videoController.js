b9863b7e8bf1bfb60a11049af5fc308bee9c043b
sheepdog13 <mikecho1234@naver.com>
2023-08-07 22:39:05 +0900

    pug, partials, extending templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,6 @@
-export const trending = (req, res) => res.send("Home");
-export const see = (req, res) => {
-    console.log(req.params);
-    res.send("See");
-};
-export const edit = (req, res) => {
-    console.log(req.params);
-    res.send("Edit")
-};
+export const trending = (req, res) => res.render("home", {pageTitle: "Home"});
+export const see = (req, res) => res.render("watch", {pageTitle: "Watch"});
+export const edit = (req, res) => res.render("edit", {pageTitle: "Edit"});
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
--- src/server.js
+++ src/server.js
@@ -8,6 +8,8 @@ const PORT = 4000;
 
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 app.use(morgan("dev"));
 
 app.use("/", globalRouter);
