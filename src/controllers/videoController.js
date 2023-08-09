8324c60276aaef8eec2636821e2386519eb6cce8
sheepdog13 <mikecho1234@naver.com>
2023-08-10 01:13:16 +0900

    pug conditionals, iteration, mixins, home view, edit view, get, post

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,11 +1,49 @@
-export const trending = (req, res) => res.render("home", {pageTitle: "Home"});
-export const see = (req, res) => res.render("watch", {pageTitle: "Watch"});
-export const edit = (req, res) => res.render("edit", {pageTitle: "Edit"});
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => {
-    console.log(req.params);
-    res.send("Delete Video");
+let videos = [
+    {
+        title: "First Video",
+        rating:5,
+        comments:2,
+        createdAt:"2 minutes ago",
+        views:59,
+        id:1,
+    },
+    {
+        title: "Second Video",
+        rating:5,
+        comments:2,
+        createdAt:"2 minutes ago",
+        views:59,
+        id:2,
+    },
+    {
+        title: "Third Video",
+        rating:5,
+        comments:2,
+        createdAt:"2 minutes ago",
+        views:59,
+        id:3,
+    }
+];
+export const trending = (req, res) => {
+    res.render("home", {pageTitle: "Home", videos})
 };
+export const watch = (req, res) => {
+    const { id } = req.params;
+    const video = videos[id-1];
+    return res.render("watch", {pageTitle: `Watching ${video.title}`, video});
+};
+export const getEdit = (req, res) => {
+    const { id } = req.params;
+    const video = videos[id-1];
+    res.render("edit", {pageTitle: `Editing: ${video.title}`, video})
+};
+export const postEdit = (req, res) => {
+    const { id } = req.params;
+    const { title } = req.body;
+    videos[id-1].title = title;
+    return res.redirect(`/videos/${id}`);
+}
+export const upload = (req, res) => res.send("Upload");
+
 
 
--- src/server.js
+++ src/server.js
@@ -11,7 +11,8 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(morgan("dev"));
-
+// express application이 form의 value들을 이해할 수 있도록 하고, 자바스크립트 형식으로 변형시켜준다
+app.use(express.urlencoded({extended:true}));
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
