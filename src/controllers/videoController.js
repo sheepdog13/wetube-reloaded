2ed6c875f2231b9c5bc7cb25089a634823fd997f
sheepdog13 <mikecho1234@naver.com>
2023-08-15 01:19:57 +0900

    mongo crud, schema속성, 정규식, 404페이지, try catch, create update

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -16,17 +16,41 @@ export const home = async (req, res) => {
     const videos = await Video.find({})
     return res.render("home", {pageTitle: "Home", videos})
 };
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
     const { id } = req.params;
-    return res.render("watch", {pageTitle: `Watching`});
+    const video = await Video.findById(id);
+    // error를 먼저처리하는게 편하다
+    if(!video){
+        // return을 설정해 다음 작동이 실행되지 않게 해야한다
+        return res.render("404", {pageTitle: "Video not found"});
+    }
+    return res.render("watch", {pageTitle: video.title, video});
 };
-export const getEdit = (req, res) => {
+
+export const getEdit = async (req, res) => {
     const { id } = req.params;
-    res.render("edit", {pageTitle: `Editing`})
+    const video = await Video.findById(id);
+    if(!video){
+        return res.render("404", {pageTitle: "Video not found"});
+    }
+    res.render("edit", {pageTitle: `Edit ${video.title}`, video})
 };
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
     const { id } = req.params;
-    const { title } = req.body;
+    const { title, description, hashtags } = req.body
+    // const video = await Video.findById(id);
+    // exists(): video전체를 받아오는게 아니라 boolean을 가져온다
+    const video = await Video.exists({ _id: id });
+    if(!video){
+        return res.render("404", {pageTitle: "Video not found"});
+    }
+    await Video.findByIdAndUpdate(id, {
+        title,
+        description,
+        hashtags: hashtags
+            .split(",")
+            .map((word)=> (word.startsWith('#') ? word : `#${word}`)),
+    })
     return res.redirect(`/videos/${id}`);
 }
 export const getUpload = (req, res) => {
@@ -36,16 +60,18 @@ export const getUpload = (req, res) => {
 export const postUpload = async (req, res) => {
     // here we will add a video to the videos array.
     const { title, description, hashtags } = req.body
-    await Video.create({
-        title,
-        description,
-        createAt: Date.now(),
-        hashtags: hashtags.split(",").map((word) => `#${word}`),
-        meta: {
-            views:0,
-            rating: 0,
-        },
-    });
+    try {
+        await Video.create({
+            title,
+            description,
+            hashtags: hashtags.split(",").map((word) => `#${word}`)
+        });
+    } catch(error){
+        return res.render("upload", {
+            pageTitle: "Upload Video",
+            errorMessage: error._message,
+        });
+    }
     return res.redirect("/");
 };
 
