f25faeef2b28e1d0f701fb11b631d1914d5537c5
sheepdog13 <mikecho1234@naver.com>
2023-08-24 00:49:12 +0900

    github social login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,7 @@
 import User from "../models/User";
-import bcrypt from "bcrypt"
+import fetch from "node-fetch";
+import bcrypt, { compareSync } from "bcrypt"
+import { json } from "express";
 
 export const getjoin = (req, res) => res.render("join", {pageTitle: "join"});
 export const postjoin = async (req, res) => {
@@ -56,6 +58,75 @@ export const postLogin = async (req, res) => {
     req.session.user = user;
     return res.redirect("/");
 };
+export const startGithubLogin = (req,res) => {
+    const baseUrl = "https://github.com/login/oauth/authorize";
+    const config = {
+        client_id: process.env.GH_CLIENT,
+        allow_signup: false,
+        // 공백을 줘야한다 공식문서 참조
+        scope:"read:user user:email"
+    }
+    const params = new URLSearchParams(config).toString();
+    const finalUrl = `${baseUrl}?${params}`;
+    return res.redirect(finalUrl);
+}
+export const finishGithubLogin = async (req,res) => {
+    const baseUrl = "https://github.com/login/oauth/access_token"
+    const config = {
+        client_id: process.env.GH_CLIENT,
+        client_secret: process.env.GH_SECRET,
+        code: req.query.code,
+    };
+    const params = new URLSearchParams(config).toString();
+    const finalUrl = `${baseUrl}?${params}`;
+    const tokenRequest = await (await fetch(finalUrl, {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+        },
+    })).json();
+    
+    if("access_token" in tokenRequest) {
+        const { access_token } = tokenRequest;
+        const apiUrl = "https://api.github.com"
+        const userData = await (await fetch(`${apiUrl}/user`, {
+            headers: {
+                Authorization: `token ${access_token}`,
+            },
+        })).json()
+        const emailData = await (await fetch(`${apiUrl}/user/emails`, {
+            headers: {
+                Authorization: `token ${access_token}`,
+            },
+        })).json()
+        const emailObj = emailData.find(
+            (email) => email.primary === true && email.verified === true
+        );
+        if(!emailObj) {
+            return res.redirect("/login");
+        }
+        const existingUser = await User.findOne({email: emailObj.email});
+        if(existingUser) {
+            req.session.loggedIn = true;
+            req.session.user = existingUser;
+            return res.redirect("/");
+        } else {
+            const user = await User.create({
+                name : userData.name ? userData.name : "Unknown",
+                username: userData.login,
+                email: emailObj.email,
+                password:"",
+                socialOnly: true,
+                location:userData.location,
+            });
+            req.session.loggedIn = true;
+            req.session.user = user;
+            return res.redirect("/");
+        }
+    } else {
+        return res.redirect("/login");
+    }
+}
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
 export const logout = (req, res) => res.send("logout");
